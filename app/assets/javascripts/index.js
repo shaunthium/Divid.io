90d34b4f83fd98af5410991e936ce59a3b9b605a
Shaun Thium <shaun.thium@gmail.com>
2016-03-22 10:48:17 +0800

    Add loading modal.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
-	$("#join-button").click(function() {
-		$("#join-form").removeClass("hide-form");
-		$("#create-form").addClass("hide-form");
-		$("html,body").animate({
-			scrollTop: ($(document).height()-750)
-		}, "slow");
-	});
+    $("#join-button").click(function() {
+        $("#join-form").removeClass("hide-form");
+        $("#create-form").addClass("hide-form");
+        $("html,body").animate({
+            scrollTop: ($(document).height() - 750)
+        }, "slow");
+    });
 
-	$("#create-button").click(function() {
-		$("#create-form").removeClass("hide-form");
-		$("#join-form").addClass("hide-form");
-		$("html,body").animate({
-			scrollTop: ($(document).height()-850)
-		}, "slow");
-	})
+    $("#create-button").click(function() {
+        $("#create-form").removeClass("hide-form");
+        $("#join-form").addClass("hide-form");
+        $("html,body").animate({
+            scrollTop: ($(document).height() - 850)
+        }, "slow");
+    });
+
+
+    $body = $("body");
+
+    $("#modal-display").click(function(e) {
+        $body.addClass("loading");
+        console.log("this wokrs");
+        e.preventDefault();
+    });
+
 });
